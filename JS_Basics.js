ee667b3c31d619b3ef03924616e6e0ecabc39cd8
GARIMA NAGDEVE <144271162+garimanagdeve1218@users.noreply.github.com>
2024-05-09 22:11:39 +0530

    Create JS_Basics.js

---
+++ JS_Basics.js
@@ -0,0 +1,19 @@
+// line below is used to print on the console window---
+console.log("hello world version 3");
+//output: hello world version 3
+
+
+
+
+//we can change the value of a variable in js unlike c,c++,java---
+let lastName = 'Nagdeve'; 
+console.log(lastName);
+
+lastName = 3;
+console.log(lastName);
+//output: Nagdeve
+//        3
+
+
+
+
