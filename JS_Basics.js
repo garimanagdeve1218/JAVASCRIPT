aeecdc1d08dd5f9b40a688f792f384e9d40d3fc2
GARIMA NAGDEVE <144271162+garimanagdeve1218@users.noreply.github.com>
2024-05-11 01:47:04 +0530

    Update JS_Basics.js

--- JS_Basics.js
+++ JS_Basics.js
@@ -22,3 +22,113 @@ let age=17;
 let status = (age>=18) ? 'Yes Vote' : 'can not vote';
 console.log(status);
 //output: can not vote
+
+
+
+
+
+//control statements---(1) if - else if - else
+let marks = 98;
+if(marks>90){
+    console.log("GRADE A")
+}
+else if(marks>80){
+    console.log('GRADE B')
+}
+else if(marks>70){
+    console.log('GRADE C')
+}
+else{
+    console.log('GRADE D')
+}
+//output: GRADE A
+
+
+
+
+
+//control statements---(2) switch-case
+//if 1-->A, 2-->B, 3-->C
+let num = 2;
+
+switch(num) {
+    case 1: console.log('GRADE A')
+    break;
+
+    case 2: console.log('GRADE B');
+    break;
+
+    case 3: console.log('GRADE C');
+    break;
+
+    default: console.log('GRADE D');
+}
+//output: GRADE B
+
+
+
+
+
+//for loop---ex(1)
+for(let i = 0; i<=5; i=i+1) {
+    console.log(i);
+}
+//output: 
+//0
+//1
+//2
+//3
+//4
+//5
+
+
+
+
+
+//for loop---ex(2)
+for(let i = 0; i<=5; i=i+1) {
+    console.log('GARIMA '+ i);
+}
+//output: 
+//GARIMA 0
+//GARIMA 1
+//GARIMA 2
+//GARIMA 3
+//GARIMA 4
+//GARIMA 5
+
+
+
+
+
+//while loop---ex(1)
+let x = 1;
+
+while(x<=5) {
+    console.log(x);
+    x++;
+}
+//output:
+//1
+//2
+//3
+//4
+//5
+
+
+
+
+
+//while loop---ex(2)
+let x = 1;
+
+while(x<=5) {
+  console.log('GARIMA ' + x)
+  x++;
+}
+//output:
+//1
+//2
+//3
+//4
+//5
